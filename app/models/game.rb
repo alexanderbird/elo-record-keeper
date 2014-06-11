fcf45626661070e289518b56dcb4151492f0688b
Alex on Macbook <alexkbird@gmail.com>
2014-06-11 00:08:00 -0600

    show "Player" or "Team" based on number of players per team ( >1 ? )

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,7 @@
 class Game < ActiveRecord::Base
   belongs_to :game_type, foreign_key: :gametype
   has_many :participations
+  def team_descriptor
+    self.game_type.players_per_team > 1 ? "Team" : "Player"
+  end
 end
